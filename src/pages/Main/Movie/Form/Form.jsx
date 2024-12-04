90cc461c497984fe583a9b71a4377b30036d9704
Kouji <Kouji@DESKTOP-VO9O7C0>
2024-12-04 21:11:58 +0800

    New Anime Data Entry in Form.jsx

--- src/pages/Main/Movie/Form/Form.jsx
+++ src/pages/Main/Movie/Form/Form.jsx
@@ -10,12 +10,13 @@ const Form = () => {
   const [anime, setAnime] = useState(undefined);
   const [page, setPage] = useState(1);
   const [pageSize] = useState(20);
+  const [selectedItem, setSelectedItem] = useState(null);
   let { animeId } = useParams();
   const navigate = useNavigate();
 
-  const userId = localStorage.getItem('userId');
+  const userId = localStorage.getItem("userId");
 
-const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+  const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
   const getCurrentPageItems = () => {
     const startIndex = (page - 1) * pageSize;
@@ -23,138 +24,198 @@ const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
     return searchedAnimeList.slice(startIndex, endIndex);
   };
 
-const handleSearch = useCallback(() => {
+  const handleSearch = useCallback(() => {
+    console.log("searching...");
 
-  console.log('searching...');
+    setPage(1);
 
-  setPage(1);
+    const apiKey = process.env.REACT_APP_TMDB_API_KEY;
+    const genreAnimationId = 16;
 
-  const apiKey = process.env.REACT_APP_TMDB_API_KEY;
-  const genreAnimationId = 16;
-
-  axios({
-    method: "get",
-    url: `https://api.themoviedb.org/3/search/multi?query=${query}&include_adult=true&original_language=ja&api_key=${apiKey}`,
-    headers: {
-      Accept: "application/json",
-    },
-  })
-    .then(async (response) => {
-      let totalPages = response.data.total_pages;
-
-      if (totalPages > 200) {
-        totalPages = 200;
-        console.log(totalPages)
-      }
-
-      const fetchAllPages = [];
-      for (let page = 1; page <= totalPages; page++) {
-        await delay(50);
-
-        fetchAllPages.push(
-          axios({
-            method: "get",
-            url: `https://api.themoviedb.org/3/search/multi?query=${query}&include_adult=false&original_language=ja&page=${page}&api_key=${apiKey}`,
-            headers: {
-              Accept: "application/json",
-            },
-          })
-        );
-      }
-
-      Promise.all(fetchAllPages)
-        .then((pageResponses) => {
-          const allResults = pageResponses.reduce((acc, currentPage) => {
-            return acc.concat(currentPage.data.results);
-          }, []);
-
-          const filteredResults = allResults.filter((item) => {
-            const hasAnimationGenre = item.genre_ids && item.genre_ids.includes(genreAnimationId);
-            const isJapanese = item.original_language === 'ja';
-            return hasAnimationGenre && isJapanese;
-          });
-
-          setsearchedAnimeList(filteredResults);
-          console.log("Filtered Results:", filteredResults);
-
-          const fetchAdditionalData = filteredResults.map((item) => {
-            const { id, media_type } = item;
-            const animeDetailsUrl = `https://api.themoviedb.org/3/${media_type}/${id}?api_key=${apiKey}&language=en-US`;
-            const videoUrl = `https://api.themoviedb.org/3/${media_type}/${id}/videos?api_key=${apiKey}&language=en-US`;
-            const castUrl = `https://api.themoviedb.org/3/${media_type}/${id}/credits?api_key=${apiKey}&language=en-US`;
-
-            return axios.all([
-              axios.get(animeDetailsUrl),
-              axios.get(videoUrl),
-              axios.get(castUrl),
-            ])
-            .then(axios.spread((animeDetails, videoDetails, castDetails) => {
-              return {
-                ...item,
-                videoKey: videoDetails.data.results.length > 0 ? videoDetails.data.results[0].key : null,
-                cast: castDetails.data.cast,
-              };
-            }));
-          });
-
-          Promise.all(fetchAdditionalData)
-            .then((animesWithAdditionalData) => {
-              console.log("animes with Additional Data:", animesWithAdditionalData);
-              setsearchedAnimeList(animesWithAdditionalData);
-            })
-            .catch((error) => {
-              console.log("Error fetching additional data:", error);
-              alert("Error fetching additional data");
-            });
-        })
-        .catch((error) => {
-          console.log("Error fetching all pages:", error);
-          alert("Error fetching all pages");
-        });
+    axios({
+      method: "get",
+      url: `https://api.themoviedb.org/3/search/multi?query=${query}&include_adult=true&original_language=ja&api_key=${apiKey}`,
+      headers: {
+        Accept: "application/json",
+      },
     })
-    .catch((error) => {
-      console.log("Error fetching search data:", error);
-      alert("Error fetching data");
-    });
-}, [query]);
+      .then(async (response) => {
+        let totalPages = response.data.total_pages;
 
-  const handleSelectanime = (anime) => {
-    setSelectedAnime(anime);
+        if (totalPages > 200) {
+          totalPages = 200;
+          console.log(totalPages);
+        }
+
+        const fetchAllPages = [];
+        for (let page = 1; page <= totalPages; page++) {
+          await delay(50);
+
+          console.log(page);
+
+          fetchAllPages.push(
+            axios({
+              method: "get",
+              url: `https://api.themoviedb.org/3/search/multi?query=${query}&include_adult=false&original_language=ja&page=${page}&api_key=${apiKey}`,
+              headers: {
+                Accept: "application/json",
+              },
+            })
+          );
+        }
+        Promise.all(fetchAllPages)
+          .then((pageResponses) => {
+            const allResults = pageResponses.reduce((acc, currentPage) => {
+              return acc.concat(currentPage.data.results);
+            }, []);
+            const filteredResults = allResults.filter((item) => {
+              const hasAnimationGenre =
+                item.genre_ids && item.genre_ids.includes(genreAnimationId);
+              const isJapanese = item.original_language === "ja";
+              return hasAnimationGenre && isJapanese;
+            });
+
+            setsearchedAnimeList(filteredResults);
+            console.log("Filtered Results:", filteredResults);
+          })
+          .catch((error) => {
+            console.log("Error fetching all pages:", error);
+            alert("Error fetching all pages");
+          });
+      })
+      .catch((error) => {
+        console.log("Error fetching search data:", error);
+        alert("Error fetching data");
+      });
+  }, [query]);
+
+  const handleSelectanime = async (anime) => {
+    const apiKey = process.env.REACT_APP_TMDB_API_KEY;
+
+    try {
+      // Fetch detailed anime data
+      const { data: animeDetails } = await axios.get(
+        `https://api.themoviedb.org/3/${anime.media_type}/${anime.id}?api_key=${apiKey}`
+      );
+
+      // Fetch videos
+      const { data: videoData } = await axios.get(
+        `https://api.themoviedb.org/3/${anime.media_type}/${anime.id}/videos?api_key=${apiKey}`
+      );
+      const videos = videoData.results.map((video) => ({
+        id: video.id,
+        key: video.key,
+        name: video.name,
+        type: video.type,
+        site: video.site,
+      }));
+
+      const { data: creditsData } = await axios.get(
+        `https://api.themoviedb.org/3/${anime.media_type}/${anime.id}/credits?api_key=${apiKey}`
+      );
+      const casts = creditsData.cast.map((cast) => ({
+        id: cast.id,
+        name: cast.name,
+        character: cast.character,
+        profile_path: cast.profile_path
+          ? `https://image.tmdb.org/t/p/original${cast.profile_path}`
+          : null,
+      }));
+
+      const { data: imagesData } = await axios.get(
+        `https://api.themoviedb.org/3/${anime.media_type}/${anime.id}/images?api_key=${apiKey}`
+      );
+
+      const posters = imagesData.posters.map((poster) => ({
+        file_path: `https://image.tmdb.org/t/p/original${poster.file_path}`,
+        width: poster.width,
+        height: poster.height,
+      }));
+      const backdrops = imagesData.backdrops.map((backdrop) => ({
+        file_path: `https://image.tmdb.org/t/p/original${backdrop.file_path}`,
+        width: backdrop.width,
+        height: backdrop.height,
+      }));
+
+      setSelectedAnime({
+        id: animeDetails.id,
+        adult: animeDetails.adult,
+        all_backdrop_path: backdrops || [],
+        episode_run_time: animeDetails.episode_run_time || animeDetails.runtime,
+        first_air_date:
+          animeDetails.first_air_date || animeDetails.release_date,
+        genres: animeDetails.genres || [],
+        homepage: animeDetails.homepage || "",
+        images: imagesData || {},
+        origin_country: animeDetails.origin_country || [],
+        original_language: animeDetails.original_language,
+        original_name:
+          animeDetails.original_name || animeDetails.original_title,
+        name: animeDetails.name || animeDetails.title,
+        overview: animeDetails.overview || "",
+        popularity: animeDetails.popularity,
+        all_poster_path: posters || [],
+        production_companies: animeDetails.production_companies || [],
+        seasons: animeDetails.seasons || null,
+        status: animeDetails.status || "",
+        vote_average: animeDetails.vote_average,
+        vote_count: animeDetails.vote_count,
+        all_videos: videos || [],
+        casts: casts || [],
+      });
+
+      console.log("Anime details fetched successfully:", selectedAnime);
+    } catch (error) {
+      console.error("Error fetching anime details:", error);
+      alert("Failed to fetch detailed anime information. Please try again.");
+    }
   };
 
   const handleSave = () => {
+    saveToAnime();
+  };
+
+  const saveToAnime = () => {
     const accessToken = localStorage.getItem("accessToken");
-    console.log(accessToken)
-  
+    console.log(accessToken);
+
     if (!accessToken) {
       alert("No access token found. Please log in.");
       return;
     }
-  
+
     if (!selectedAnime) {
       alert("Please search and select an anime.");
       return;
     }
-  
+
+    if (!selectedAnime.poster_path || !selectedAnime.backdrop_path) {
+      alert("Selected anime must have a poster and backdrop.");
+      return;
+    }
+
     const data = {
-      adult: selectedAnime?.adult,
-      backdrop_path: `https://image.tmdb.org/t/p/original/${selectedAnime?.backdrop_path}`,
-      cast: selectedAnime?.cast,
-      genre_ids: selectedAnime?.genre_ids,
       tmdbId: selectedAnime?.id,
-      media_type: selectedAnime?.media_type,
-      name: selectedAnime?.title || selectedAnime?.name,
-      original_language: selectedAnime?.original_language,
-      original_name: selectedAnime?.original_name || selectedAnime?.original_title,
-      overview: selectedAnime?.overview,
-      popularity: selectedAnime?.popularity,
-      poster_path: `https://image.tmdb.org/t/p/original/${selectedAnime?.poster_path}`,
-      release_date: selectedAnime?.release_date || selectedAnime?.first_air_date,
-      videoKey: selectedAnime?.videoKey,
-      vote_average: selectedAnime?.vote_average,
-      vote_count: selectedAnime?.vote_count,
+      adult: selectedAnime?.adult,
+      backdrop_path: selectedAnime?.backdrop_path || null,
+      episode_run_time: selectedAnime?.episode_run_time || null,
+      first_air_date: selectedAnime?.first_air_date || null,
+      genres: selectedAnime?.genres || null,
+      homepage: selectedAnime?.homepage || null,
+      origin_country: selectedAnime?.origin_country || null,
+      original_language: selectedAnime?.original_language || null,
+      original_name: selectedAnime?.original_name || null,
+      name: selectedAnime?.name || null,
+      overview: selectedAnime?.overview || null,
+      popularity: selectedAnime?.popularity || null,
+      poster_path: selectedAnime?.poster_path || null,
+      production_companies: selectedAnime?.production_companies || null,
+      seasons: selectedAnime?.seasons || null,
+      status: selectedAnime?.status || null,
+      vote_average: selectedAnime?.vote_average || null,
+      vote_count: selectedAnime?.vote_count || null,
     };
-  
+
     axios({
       method: "post",
       url: "/animeCrud.php",
@@ -173,28 +234,14 @@ const handleSearch = useCallback(() => {
         alert("Error saving data");
       });
   };
-  
 
-  // useEffect(() => {
-  //   if (animeId) {
-  //     axios.get(`/animes/${animeId}`).then((response) => {
-  //       setAnime(response.data);
-  //       const tempData = {
-  //         id: response.data.tmdbId,
-  //         original_title: response.data.title,
-  //         overview: response.data.overview,
-  //         popularity: response.data.popularity,
-  //         poster_path: response.data.posterPath,
-  //         release_date: response.data.releaseDate,
-  //         vote_average: response.data.voteAverage,
-  //       };
-  //       setSelectedAnime(tempData);
-  //     });
-  //   }
-  // }, []);
+  const handleSelect = (item, type) => {
+    setSelectedItem({ ...item, type });
+    setSelectedAnime({ ...selectedAnime, [`${type}`]: item });
+    console.log(selectedAnime);
+  };
 
   const totalPages = Math.ceil(searchedAnimeList.length / pageSize);
-
 
   return (
     <>
@@ -224,13 +271,19 @@ const handleSearch = useCallback(() => {
             {searchedAnimeList.length > 0 && (
               <>
                 <div className="pagination">
-                  <button onClick={() => setPage(page - 1)} disabled={page === 1}>
+                  <button
+                    onClick={() => setPage(page - 1)}
+                    disabled={page === 1}
+                  >
                     Previous
                   </button>
                   <span>
                     Page {page} of {totalPages}
                   </span>
-                  <button onClick={() => setPage(page + 1)} disabled={page === totalPages}>
+                  <button
+                    onClick={() => setPage(page + 1)}
+                    disabled={page === totalPages}
+                  >
                     Next
                   </button>
                 </div>
@@ -243,11 +296,18 @@ const handleSearch = useCallback(() => {
         <div className="container">
           <form>
             {selectedAnime ? (
-              <img
-                className="poster-image"
-                src={`https://image.tmdb.org/t/p/original/${selectedAnime.poster_path}`}
-                alt={selectedAnime.original_title}
-              />
+              <>
+                <img
+                  src={selectedAnime.backdrop_path}
+                  alt={selectedAnime.name}
+                  width="300"
+                />
+                <img
+                  className="poster-image"
+                  src={`https://image.tmdb.org/t/p/original/${selectedAnime.poster_path}`}
+                  alt={selectedAnime.original_title}
+                />
+              </>
             ) : (
               ""
             )}
@@ -256,7 +316,9 @@ const handleSearch = useCallback(() => {
               <input
                 type="text"
                 disabled={!animeId}
-                value={selectedAnime ? selectedAnime.title || selectedAnime.name : ""}
+                value={
+                  selectedAnime ? selectedAnime.title || selectedAnime.name : ""
+                }
                 onChange={(e) =>
                   setSelectedAnime({
                     ...selectedAnime,
@@ -272,7 +334,10 @@ const handleSearch = useCallback(() => {
                 rows={10}
                 value={selectedAnime ? selectedAnime.overview : ""}
                 onChange={(e) =>
-                  setSelectedAnime({ ...selectedAnime, overview: e.target.value })
+                  setSelectedAnime({
+                    ...selectedAnime,
+                    overview: e.target.value,
+                  })
                 }
               />
             </div>
@@ -295,7 +360,11 @@ const handleSearch = useCallback(() => {
               <input
                 type="text"
                 disabled={!animeId}
-                value={selectedAnime ? selectedAnime.release_date || selectedAnime.first_air_date : ""}
+                value={
+                  selectedAnime
+                    ? selectedAnime.release_date || selectedAnime.first_air_date
+                    : ""
+                }
                 onChange={(e) =>
                   setSelectedAnime({
                     ...selectedAnime,
@@ -323,14 +392,79 @@ const handleSearch = useCallback(() => {
             </button>
           </form>
         </div>
-        
+      </div>
+
+      <div>
+        <h1>{selectedAnime?.name || "Anime Details"}</h1>
+
+        {/* Videos Section */}
+        <div>
+          <h2>Videos</h2>
+          <div className="videos-list">
+            {selectedAnime?.videos?.map((video) => (
+              <div key={video.id} className="video-item">
+                <p>
+                  <strong>{video.name}</strong> ({video.type})
+                </p>
+                <p>Platform: {video.site}</p>
+                <button onClick={() => handleSelect(video, "video")}>
+                  Select Video
+                </button>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        {/* Posters Section */}
+        <div>
+          <h2>Posters</h2>
+          <div className="posters-list">
+            {selectedAnime?.all_poster_path?.map((poster, index) => (
+              <div key={index} className="poster-item">
+                <img
+                  src={poster.file_path}
+                  alt={`Poster ${index}`}
+                  width="200"
+                />
+                <button
+                  onClick={() => handleSelect(poster.file_path, "poster_path")}
+                >
+                  Select Poster
+                </button>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        {/* Backdrops Section */}
+        <div>
+          <h2>Backdrops</h2>
+          <div className="backdrops-list">
+            {selectedAnime?.all_backdrop_path?.map((backdrop, index) => (
+              <div key={index} className="backdrop-item">
+                <img
+                  src={backdrop.file_path}
+                  alt={`Backdrop ${index}`}
+                  width="300"
+                />
+                <button
+                  onClick={() =>
+                    handleSelect(backdrop.file_path, "backdrop_path")
+                  }
+                >
+                  Select Backdrop
+                </button>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
 
       {animeId !== undefined && selectedAnime && (
         <div>
           <hr />
           <nav>
-            <ul className='tabs'>
+            <ul className="tabs">
               <li
                 onClick={() => {
                   navigate(`/main/animes/form/${animeId}/cast-and-crews`);
